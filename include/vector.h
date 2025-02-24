55e82b3073448cc7ab92f3f455f4476e4af614b6
Jamie Nash <jnash1997@gmail.com>
2025-02-24 21:12:32 +0100

    Added vector header file -- still to be completed and to create associated implementation file

---
+++ include/vector.h
@@ -0,0 +1,35 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+namespace vector{
+
+    /* Create vector class with variable dimension sizing
+    Template will cause the compiler to create the class as necessary
+    for different values of Dimension, allowing for multiple vectors 
+    of different dimensions to be created with this single class.
+    In this case, Dimension is the tempate parameter.
+    Vector<int, 2> will create a 2D vector of integers
+    Vector<std::string, 3> will create a 3D vector of strings etc.*/
+    template<typename T, int Dimension>
+    class Vector {
+        // Below is TBC
+        // private parameters that will not be changed
+        private:
+            
+
+        // public accessible parameters
+        public:
+        
+        
+
+
+        
+
+
+
+
+};
+
+
+}
+#endif
