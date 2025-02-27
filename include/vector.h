8a1847dcb0059f2573f882117001685f1e80da47
Jamie Nash <jnash1997@gmail.com>
2025-02-27 22:08:28 +0100

    added operator[] functionality

--- include/vector.h
+++ include/vector.h
@@ -24,7 +24,11 @@ namespace vector{
 
         // public accessible parameters
         public:
-            // Constructors
+            /* Constructors 
+            Note that the Vector function is defined thrice. As all 3
+            instances of this function require different inputs, the 
+            compiler is still able to differentiate between each of 
+            these functions. This is called overloading the function.*/
 
             /* Allows for Vector command with no input variables to create a 
             default vector if zeros*/
@@ -49,16 +53,36 @@ namespace vector{
             altering the original.*/
             Vector(const Vector<T, Dimension>& other){
                 for(int i = 0; i < Dimension; i++){
-                    /* Still requires operator[] definition to access components 
-                    of a given index; this implementation will not work yet!!! */
                     components[i] = other[i];
                 }
             };
 
+            /* Create a definition of the [] operator for our vector class.
+            i.e. if we have an int vector vec, we could store the 1st value of that
+            vector by running: int x = vec[0];
+            Similarly we can alter the values of vec by running vec[0] = int(10);
+            This is because we used the & operator, which tells the operator that we 
+            are calling a reference to the actual value of the return value.
+            In other words, [] will return the address where the variable is stored
+            opposed to making a new parameter with the same value.*/
+            T& operator[](int index){
+                return components[index];
+            }
+
+            /* As the above allows for direct editing of a vector, it cannot be used 
+            when a vector is defined as a const. This is why we need the following 
+            version of the operator definintion to allow for pulling a specific index
+            from a const vector. The first const allows for viewing of the reference
+            but prevents editing (i.e protects the vector element). The second const 
+            tells the compiler that the method of this operator will not edit the vector
+            that the operator is called on (i.e. protects the vector as a whole).*/
+            const T& operator[](int index) const {
+                return components[index];
+            }
 
 
 };
 
 
-}
+} // namespace vector
 #endif
